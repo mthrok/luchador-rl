af1afce57b7df1834369c1210015f4103c7487e0
moto (・_・) <mthrok.ca@gmail.com>
2017-03-25 14:30:02 -0700

    Print individual error in vae example

--- tests/integration/run_autoencoder.sh
+++ tests/integration/run_autoencoder.sh
@@ -9,4 +9,4 @@ if [ ! -f "${DATA}" ]; then
     curl -o "${DATA}" http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
 fi
 python example/autoencoder/run_autoencoder.py --mnist "${DATA}" --model example/autoencoder/autoencoder.yml  --no-plot
-python example/autoencoder/run_autoencoder.py --mnist "${DATA}" --model example/autoencoder/variational_autoencoder.yml  --no-plot
+python example/autoencoder/run_variationalautoencoder.py --mnist "${DATA}" --model example/autoencoder/variational_autoencoder.yml  --no-plot
