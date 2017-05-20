99c60af3493526f37dc1fec18b3fd95b3f662f23
moto <mthrok@gmail.com>
2017-05-20 00:13:00 -0700

    Fix test

--- tests/integration/run_dqn.sh
+++ tests/integration/run_dqn.sh
@@ -7,7 +7,7 @@ DATA_DIR="${BASE_DIR}/data/dqn"
 if [ "${COUNT_INTEGRATION_COVERAGE:-false}" = true ]; then
     TEST_COMMAND="coverage run --parallel-mode tool/profile.py"
 else
-    TEST_COMMAND="luchador"
+    TEST_COMMAND="luchador_rl"
 fi
 
 ${TEST_COMMAND} exercise "${DATA_DIR}/ALEEnvironment_train.yml" --agent "${DATA_DIR}/DQNAgent_train.yml" --episodes 10 --steps 500
